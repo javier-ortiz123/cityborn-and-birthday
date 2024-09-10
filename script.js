7d5b136baa154309f3af96960c05a138d40e7bdc
javier <156357273+javier-ortiz123@users.noreply.github.com>
2024-09-10 14:46:56 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Write your JavaScript below this line
+var cityBorn = "Wilkes-Barre";
+var birthday = "May 14 1900";
+
+console.log("I was born in " + "Wilkes-Barre" + " on " + "May 14 1900" + ".");
